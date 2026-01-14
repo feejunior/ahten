6a76aaff07df0f599a8c2b7763e41249cb5512e7
FJ <felipegd.jr@gmail.com>
2026-01-14 00:14:23 -0300

    database again

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from app.routes.health import router as health_router
 from app.routes.psicologas import router as psicologas_router
+from app.db.database import engine, Base
+from app.models import psicologa
+
 
 app = FastAPI(title="Ahten Psico")
 
 app.include_router(health_router)
 app.include_router(psicologas_router)
+Base.metadata.create_all(bind=engine)
+
